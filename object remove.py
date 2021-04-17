963dfec454b36b8eb8686e50d5c206eb36174cd3
DESKTOP-1DN5T5V\JUHWAN <st3119@gmail.com>
2021-04-18 00:38:51 +0900

    add gif, clean code

--- object remove.py
+++ object remove.py
@@ -2,26 +2,20 @@ import numpy as np
 from PIL import Image
 import cv2 as cv2
 import os
+from matcher import stackImagesKeypointMatching
 def remove_obj(video_frame):
-    # Load images
-
-    # im0 = np.array(Image.open('1.jpg'))
-    # im1 = np.array(Image.open('2.jpg'))
-    # im2 = np.array(Image.open('3.jpg'))
-
-    # Stack the 3 images into a 4d sequence
+    
     sequence = video_frame[0]
     results = video_frame[0]
-    for i in range(len(video_frame)-2):
-        print(i)
-        print(sequence.shape)
-        print(video_frame[i+1].shape)
+    
+    
+    
+    for i in range(len(video_frame)-2):        
         sequence = np.stack((sequence, video_frame[i+1]), axis=3)
         result = np.median(sequence, axis=3).astype(np.uint8)
         sequence = result
         results = result
     
-
     # Save to disk
     Image.fromarray(results).save('result.png')
 
@@ -33,7 +27,7 @@ def video2image(input_path):
     
     list_dir = os.listdir(input_path)
 
-    # vidcap = cv2.VideoCapture(input_path + list_dir[0])
+    
     fname = list_dir[0]
     vidcap = cv2.VideoCapture(input_path + fname)
 
@@ -61,10 +55,11 @@ def video2image(input_path):
         if (int(vidcap.get(1))):
             # l_time = time.time()
             
-            # cv2.imwrite("./output/" + "%d.png" % count, image)
+            
             video_frame[count] = image            
-            # print('Saved frame%d.jpg' % count)
+            
             count += 1
+            
             #for bandicam cut 4 sec
             # if(int(vidcap.get(1)) >= v_length -int(v_fps * 4)):
             if(int(vidcap.get(1)) >= v_length-1):
@@ -74,8 +69,12 @@ def video2image(input_path):
     print(len(video_frame))
     
     return video_frame
-    
-video_frame = video2image("./input/")
 
-remove_obj(video_frame)
+
+def main():
+    video_frame = video2image("./input/")    
+    remove_obj(video_frame)
+
+if __name__ == "__main__":    
+    main()
 
